6356e2c900ce0a26e5118a073865b4a62518a833
Jarrod <gatejarr@gmail.com>
2024-01-08 18:01:56 -0500

    WIP; word count working, but is counting the s after an apostrophy as a word in some files, adding it to words in others. Also need to sort the words from most to least often, and add the asterisk after the count

--- word_frequency.js
+++ word_frequency.js
@@ -1,48 +1,66 @@
-const fs = require('fs')
-const path = require('path')
-const filePath = process.argv[2]
+const fs = require("fs");
+const path = require("path");
+const filePath = process.argv[2];
 
 const STOP_WORDS = [
-  'a',
-  'an',
-  'and',
-  'are',
-  'as',
-  'at',
-  'be',
-  'by',
-  'for',
-  'from',
-  'has',
-  'he',
-  'i',
-  'in',
-  'is',
-  'it',
-  'its',
-  'of',
-  'on',
-  'that',
-  'the',
-  'to',
-  'were',
-  'will',
-  'with',
-]
+  "a",
+  "an",
+  "and",
+  "are",
+  "as",
+  "at",
+  "be",
+  "by",
+  "for",
+  "from",
+  "has",
+  "he",
+  "i",
+  "in",
+  "is",
+  "it",
+  "its",
+  "of",
+  "on",
+  "that",
+  "the",
+  "to",
+  "were",
+  "will",
+  "with",
+];
 
 function printWordFreq(file, callback) {
   // Read in `file` and print out the frequency of words in that file.
-  fs.readFile(file, 'utf8', (err, data) => {
+  fs.readFile(file, "utf8", (err, data) => {
     if (err) {
-      console.error('Error reading the file:', err)
-      process.exit(1)
+      console.error("Error reading the file:", err);
+      process.exit(1);
     }
     // TODO: write code to count the words in the file
-    console.log('Initial data read from file: ', data)
-    callback('Your word count results should be passed in to this callback')
-  })
+
+    const wordCount = data
+      .replace(/[.,'\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+      .toLowerCase()
+      .split(/\W+/);
+    // .sort();
+    const frequency = [];
+
+    for (const word of wordCount) {
+      if (STOP_WORDS.includes(word)) {
+        continue;
+      }
+      if (word in frequency) {
+        frequency[word]++;
+      } else {
+        frequency[word] = 1;
+      }
+    }
+    // console.log("Initial data read from file: ", data);
+    callback(frequency);
+  });
 }
 
 printWordFreq(filePath, (wordCount) => {
-  console.log('The results from your word counts:', wordCount)
-})
+  console.log("The results from your word counts:", wordCount);
+});
